d68877b2087cec4ccc8ac4f6ed5c3ff2736c5595
jv185150 <jv185150@ncr.com>
2021-10-23 22:47:17 -0400

    update graph

--- lib/card.dart
+++ lib/card.dart
@@ -20,11 +20,18 @@ class _AssetCardState extends State<AssetCard> {
   Color fcolor = Colors.grey;
   bool isActive = false;
   int activeIndex = 0;
+  List<double> xValues = [];
   List<DataPoint> dataPoints = [];
 
   getDataPoints() {
     for (int i = 0; i < widget.asset.priceData.close!.length; i++) {
-      dataPoints.add(DataPoint(value: widget.asset.priceData.close![i].toDouble(), xAxis: widget.asset.priceData.timestamp![i]));
+      double price = widget.asset.priceData.close![i] == null ? 0.0 : widget.asset.priceData.close![i].toDouble();
+      double timestamp = widget.asset.priceData.timestamp![i].toDouble();
+      if (price != null && price > 0) {
+        print('${widget.asset.name}, ${widget.asset.data.closePrice}, $timestamp, $price');
+        xValues.add(i.toDouble());
+        dataPoints.add(DataPoint(value: price, xAxis: timestamp));
+      }
     }
     setState(() {
       dataPoints = dataPoints;
@@ -128,23 +135,15 @@ class _AssetCardState extends State<AssetCard> {
                   child: BezierChart(
                     bezierChartScale: BezierChartScale.CUSTOM,
                     selectedValue: 1,
-                    xAxisCustomValues: [1, 5, 10, 15, 20, 25, 30],
-                    series: const [
+                    xAxisCustomValues: xValues,
+                    series: [
                       BezierLine(
                         label: "june",
                         lineColor: Colors.orange,
                         dataPointStrokeColor: Colors.orange,
                         dataPointFillColor: Colors.orange,
                         lineStrokeWidth: 3,
-                        data: const [
-                          DataPoint<double>(value: 100, xAxis: 1),
-                          DataPoint<double>(value: 130, xAxis: 5),
-                          DataPoint<double>(value: 300, xAxis: 10),
-                          DataPoint<double>(value: 150, xAxis: 15),
-                          DataPoint<double>(value: 75, xAxis: 20),
-                          DataPoint<double>(value: 100, xAxis: 25),
-                          DataPoint<double>(value: 250, xAxis: 30),
-                        ],
+                        data: dataPoints,
                       ),
                     ],
                     config: BezierChartConfig(
